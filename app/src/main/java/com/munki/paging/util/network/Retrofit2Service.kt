6054cef5589492e6ed575bae94b68b600c596859
정문기 <munkijung1992@gmail.com>
2021-03-08 17:13:31 +0900

    불필요 Java 파일 삭제

--- app/src/main/java/com/munki/paging/util/network/Retrofit2Service.kt
+++ app/src/main/java/com/munki/paging/util/network/Retrofit2Service.kt
@@ -1,11 +1,5 @@
 package com.munki.paging.util.network
 
-import com.munki.paging.data.GitResult
-import io.reactivex.Observable
-import retrofit2.Response
-import retrofit2.http.GET
-import retrofit2.http.Query
-
 /**
  * Retrofit2 + Rxjava2 -> Interface
  * @author 나비이쁜이
